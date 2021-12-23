5e31e905ac8518babc10077af73df02f7b0ad6ce
krister <looga.krister@gmail.com>
2021-12-23 06:01:51 +0200

    changed day_16 logic to recursive

--- day_16/index.js
+++ day_16/index.js
@@ -26,102 +26,63 @@ const decodeString = (string) => {
 const binaryToDecimal = (string) => {
     return parseInt(string, 2);
 }
+const operations = {
+    "0": (arr) => [arr.reduce((acc,curr) => acc + curr, 0)],
+    "1": (arr) => [arr.reduce((acc, curr) => acc * curr, 1)],
+    "2": (arr) => [Math.min(...arr)],
+    "3": (arr) => [Math.max(...arr)],
+    "4": (arr) => arr,
+    "5": (arr) => [arr[0] > arr[1] ? 1 : 0],
+    "6": (arr) => [arr[0] < arr[1] ? 1 : 0],
+    "7": (arr) => [arr[0] === arr[1] ? 1 : 0],
+}
 
-const literalValue = (string, subPacketCount, subPacketLength) => {
-    let counter = 0;
-    let result = [];
-    let versionCounter = 0;
-    let currentResult = "";
-    while (true) {
-        const currentString = string.substring(counter * 5, (counter + 1) * 5);
-        const startCharacter = currentString.substring(0, 1);
-        const currentInput = currentString.substring(1);
-        currentResult += currentInput
-        if (startCharacter === "0") {
-            if (subPacketLength !== Infinity) { subPacketLength -= currentResult.length }
-            const [value, versions] = packetParser({ packet: string.substring((counter + 1) * 5), subPacketCount, subPacketLength});
-            result = [currentResult, ...value];
-            versionCounter += versions;
-            break;
+const recursiveParser = (input, startingIndex) => {
+    let nextStartingIndex = startingIndex;
+    let currentVersion = +binaryToDecimal(input.substring(startingIndex, startingIndex + 3));
+    const currentType = +binaryToDecimal(input.substring(startingIndex + 3,startingIndex + 6));
+    nextStartingIndex += 6 // shift by six
+    let values = []
+
+    if (currentType === 4) { // handle literal
+        let result = "";
+        while (true) {
+            const currentBits = input.substring(nextStartingIndex, nextStartingIndex + 5);
+            const startingBit = currentBits[0];
+            nextStartingIndex += 5;
+            result += currentBits.substring(1);
+            if (startingBit === "0" || currentBits === '') break;
         }
-        counter++;
-    }
-
-    return [result, versionCounter];
-}
-
-const operatorValue = (string, subPacketCount) => {
-    const lengthId = string.substring(0, 1);
-    const parseString = string.substring(1)
-    let answer = [];
-    let versionCounter = 0;
-    console.log({parseString, lengthId})
-    if (lengthId === "0") {
-        const lengthOfSubPackets = +binaryToDecimal(parseString.substring(0, 15));
-        const [answer1, versionCounter1] = packetParser({packet: parseString.substring(15, 15 + lengthOfSubPackets), subPacketLength: lengthOfSubPackets})
-        answer = [...answer, ...answer1];
-        versionCounter += versionCounter1;
-        if (subPacketCount > 0) {
-            const [answer2, versionCounter2] = packetParser({packet: parseString.substring(15 + lengthOfSubPackets)})
-            answer = [...answer, ...answer2];
-            versionCounter += versionCounter2;
+        values.push(+binaryToDecimal(result));
+    } else { // handle operator
+        const lengthType = input[nextStartingIndex];
+        nextStartingIndex += 1;
+        if (lengthType === "0") {
+            let length = +binaryToDecimal(input.substring(nextStartingIndex, nextStartingIndex + 15));
+            nextStartingIndex += 15;
+            length += nextStartingIndex; // we create endIndex by adding the startingIndex to length
+            while (nextStartingIndex < length) {
+                const [newStartingIndex, newVersionCount, newValue] = recursiveParser(input, nextStartingIndex);
+                nextStartingIndex = newStartingIndex;
+                currentVersion += newVersionCount;
+                values = [...values, ...newValue];
+            }
+        } else {
+            const numberOfPackets = +binaryToDecimal(input.substring(nextStartingIndex, nextStartingIndex + 11));
+            nextStartingIndex += 11
+            for (let i = 0; i < numberOfPackets; i++) {
+                const [newStartingIndex, newVersionCount, newValue] = recursiveParser(input, nextStartingIndex);
+                nextStartingIndex = newStartingIndex;
+                currentVersion += newVersionCount;
+                values = [...values, ...newValue];
+            }
         }
-    } else {
-        const lengthOfSubPackets = +binaryToDecimal(parseString.substring(0, 11));
-        console.log(lengthOfSubPackets)
-        const newParseString = parseString.substring(11);
-        [answer, versionCounter] = packetParser({ packet: newParseString, subPacketCount: lengthOfSubPackets});
-        // for (let i = 0; i < lengthOfSubPackets; i++) {
-        //     answer = [...answer, value];
-        //     versionCounter += versions
-        // }
     }
-
-    return [answer, versionCounter]
+    return [nextStartingIndex, currentVersion, operations[`${currentType}`](values)]
 }
 
-const startParser = (input) => {
-    const decodedInput = decodeString(input[0]);
-    const [result, versionCounter] = packetParser({ packet: decodedInput});
-    console.log({versionCounter});
-    console.log(result.map(el => binaryToDecimal(el)));
-    return result.map(el => binaryToDecimal(el));
+const start = (input) => {
+    console.log(recursiveParser(decodeString(input[0]),0));
 }
 
-const packetParser = ({packet, subPacketCount = Infinity, subPacketLength = Infinity, versionCounter = 0}) => {
-    versionCounter += binaryToDecimal(packet.substring(0, 3));
-    const typeId = binaryToDecimal(packet.substring(3, 6));
-    const parseInput = packet.substring(6);
-    if (!parseInput || subPacketCount === 0 || subPacketLength === 0) {
-        console.log({subPacketCount, subPacketLength, parseInput})
-        return [[], 0];
-    }
-    if (subPacketCount !== Infinity) {
-        subPacketCount--
-    }
-
-    console.log("packet parser", {parseInput, versionCounter, typeId, subPacketCount, subPacketLength });
-    let value, versions;
-    if (typeId === 4) {
-        [value, versions] = literalValue(parseInput, subPacketCount, subPacketLength);
-    } else {
-        [value, versions] = operatorValue(parseInput, subPacketCount, subPacketLength);
-    }
-
-    versionCounter += versions
-    return [value, versionCounter];
-}
-
-fileReader("input.txt", startParser);
-//
-// const testInput1 = ["D2FE28"];
-// console.assert(JSON.stringify(startParser(testInput1)) === JSON.stringify([ 2021 ]))
-//
-// const testInput2 = ["38006F45291200"];
-// console.assert(JSON.stringify(startParser(testInput2)) === JSON.stringify([ 10, 20 ]))
-//
-// const testInput3 = ["EE00D40C823060"];
-// console.assert(JSON.stringify(startParser(testInput3)) === JSON.stringify([ 1,2,3 ]))
-//
-// const testInput4 = ["8A004A801A8002F478"];
-// console.assert(JSON.stringify(startParser(testInput4)) === JSON.stringify([ 1,2,3 ]))
+fileReader("input.txt", start);
